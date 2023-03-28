922b5fd0eb57eef971e459e71773e0f0aac3f59c
Karla Azucena <karla29.vazquezl@gmail.com>
2023-03-28 09:00:09 -0600

    Actualizar

--- resources/views/backend/profile/form.blade.php
+++ resources/views/backend/profile/form.blade.php
@@ -128,6 +128,20 @@
             {{ Form::hidden('qr_id',$data->qr_id, array()) }}
         @endif
 
+        <div class="card-body">
+            <div class="form-group row">
+                <div class="col-sm-2 col-form-label">
+                    <strong class="field-title">Código QR</strong>
+                </div>
+
+                <div class="col-sm-10 col-content">
+                    {{ Form::text('name', $data->name, array('class' => 'form-control', 'required')) }}
+                    <small class="form-text text-muted">
+                        <i class="fa fa-question-circle" aria-hidden="true"></i> Dowload.
+                    </small>
+                </div>
+            </div>
+
         <div class="card-footer">
             <div id="form-button">
                 <div class="col-sm-12 text-center top20">
