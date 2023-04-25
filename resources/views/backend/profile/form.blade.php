997a752b90e573242723d1247a934ace7ef5a168
Karla Azucena <karla29.vazquezl@gmail.com>
2023-04-25 09:28:07 -0600

    Actualizar

--- resources/views/backend/profile/form.blade.php
+++ resources/views/backend/profile/form.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 <!-- page title -->
-@section('title', 'Editar Perfil | ' . Config::get('adminlte.title'))
+@section('title', 'Edit Profile | ' . Config::get('adminlte.title'))
 
 @section('content_header')
-    <h1>Perfil</h1>
+    <h1>Profile</h1>
 @stop
 
 @section('content')
@@ -12,107 +12,62 @@
 
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">Actualizar</h3>
-     </div>
+            <h3 class="card-title">Update</h3>
+        </div>
 
         {{ Form::open(array('url' => route($data->form_action), 'method' => 'POST','autocomplete' => 'off', 'files' => true)) }}
         {{ Form::hidden('id', $data->id, array('id' => 'user_id')) }}
 
         <div class="card-body">
-        <div class="form-group row">
-        <div class="col-sm-2 col-form-label">
-         <strong class="field-title">Nombre</strong>
-        </div>
-
-         <div class="col-sm-10 col-content">
-             {{ Form::text('name', $data->name, array('class' => 'form-control', 'required')) }}
-            <small class="form-text text-muted">
-                <i class="fa fa-question-circle" aria-hidden="true"></i> Escribir su nombre.
-              </small>
-            </div>
-            </div>
-        </div>
-         <div class="card-body">
-        <div class="form-group row">
-        <div class="col-sm-2 col-form-label">
-         <strong class="field-title">N° Control</strong>
-        </div>
-
-         <div class="col-sm-10 col-content">
-             {{ Form::text('n control', $data->name, array('class' => 'form-control', 'required')) }}
-            <small class="form-text text-muted">
-                <i class="fa fa-question-circle" aria-hidden="true"></i> Establecer el número de control.
-              </small>
-            </div>
-            </div>
-           
-            <div class="card-body">
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Carrera</strong>
+                    <strong class="field-title">Name</strong>
                 </div>
-                <ul>
-
                 <div class="col-sm-10 col-content">
-                    {{ Form::text('carrera', $data->name, array('class' => 'form-control', 'required')) }}
+                    {{ Form::text('name', $data->name, array('class' => 'form-control', 'required')) }}
                     <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> Establecer la carrera.
-                    </small>
-                    
-                  
-                </div>
-            </div>
-            <div class="card-body">
-            <div class="form-group row">
-                <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Semestre</strong>
-                </div>
-               
-              
-                    <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> Actualizar el semestre actual.
+                        <i class="fa fa-question-circle" aria-hidden="true"></i> Your name.
                     </small>
                 </div>
             </div>
-            
 
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Correo</strong>
+                    <strong class="field-title">Email</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     {{ Form::email('email',$data->email, array('class' => 'form-control', 'required')) }}
                     <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> Su correo electrónico este correo electrónico para iniciar sesión.
+                        <i class="fa fa-question-circle" aria-hidden="true"></i> Your email, this email for login.
                     </small>
                 </div>
             </div>
 
             <div id="form-password" class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Contraseña</strong>
+                    <strong class="field-title">Password</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     {{ Form::password('password', array('id' => 'password', 'class' => 'form-control', 'autocomplete' => 'new-password')) }}
                       <small id="passwordHelpBlock" class="form-text text-muted">
-                      Su contraseña, esta contraseña para iniciar sesión. Déjalo en blanco si no quieres cambiar.    
+                          Your password, this password for login. Leave it blank if you don't want to change
                       </small>
-                    <label class="reset-field-password" for="show-password"><input id="show-password" type="checkbox" name="show-password" value="1"> Mostrar Contraseña</label>
+                    <label class="reset-field-password" for="show-password"><input id="show-password" type="checkbox" name="show-password" value="1"> Show Password</label>
                 </div>
             </div>
 
             {{--  image  --}}
             <div id="form-image" class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Imagen</strong>
+                    <strong class="field-title">Image</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     <input class="custom-file-input" name="image" type="file"
                            accept="image/gif, image/jpeg,image/jpg,image/png" data-max-width="800"
                            data-max-height="400">
-                    <label class="custom-file-label" for="customFile">Actualizar Imagen</label>
+                    <label class="custom-file-label" for="customFile">Choose file</label>
                     <span
-                        class="image-upload-label"><i class="fa fa-question-circle" aria-hidden="true"></i> Por favor, sube la imagen (tamaño recomendado: 160px × 160px, max 5MB)</span>
+                        class="image-upload-label"><i class="fa fa-question-circle" aria-hidden="true"></i> Please upload the image (Recommended size: 160px × 160px, max 5MB)</span>
                     <div class="image-preview-area">
                         <div id="image_preview" class="image-preview">
                                 <img src="{{ asset('uploads/'.$data->image) }}" width="160" title="image"
@@ -131,13 +86,6 @@
         @if($data->role == 2 || $data->role == 3)
             {{ Form::hidden('qr_id',$data->qr_id, array()) }}
         @endif
-
-        <div class="card-body">
-            <div class="form-group row">
-                <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Código QR</strong>
-                </div>
-
 
         <div class="card-footer">
             <div id="form-button">
