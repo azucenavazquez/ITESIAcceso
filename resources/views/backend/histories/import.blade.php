1ee3ba92d778150dd034a867e25cc97848af2194
Karla Azucena <karla29.vazquezl@gmail.com>
2023-04-28 09:33:14 -0600

    Actualizar

--- resources/views/backend/histories/import.blade.php
+++ resources/views/backend/histories/import.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Import Data Qr ' . Config::get('adminlte.title'))
 
 @section('content_header')
-    <h1>Import Data Qr</h1>
+    <h1>Importar </h1>
 @stop
 
 @section('content')
@@ -12,7 +12,7 @@
 
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">Import</h3>
+            <h3 class="card-title">Importar</h3>
         </div>
 
         {{ Form::open(array('url' => route($data->form_action), 'method' => 'POST','autocomplete' => 'off', 'files' => true)) }}
@@ -22,37 +22,37 @@
 
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Import Data</strong>
+                    <strong class="field-title">Importar Datos</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     <input type="file" class="custom-file-input" name="import" id="import" required>
                     <label class="custom-file-label" for="customFile">Choose file</label>
-                    <span class="image-upload-label"><i class="fa fa-question-circle" aria-hidden="true"></i> Please upload the csv File</span>
+                    <span class="image-upload-label"><i class="fa fa-question-circle" aria-hidden="true"></i> Por favor descargar archivo en csv</span>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Download Template</strong>
+                    <strong class="field-title">Descargar plantilla</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     <a href="{{ asset('img/template.csv') }}" download="">
-                      <button type="button" class="btn btn-success"> Download Template CSV</button>
+                      <button type="button" class="btn btn-success"> Descargar archivo en  CSV</button>
                     </a>
                     <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> Download CSV file template.
+                        <i class="fa fa-question-circle" aria-hidden="true"></i> Descargar plantilla en CSV.
                     </small>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Instructions</strong>
+                    <strong class="field-title">Instruciones</strong>
                 </div>
                 <div class="col-sm-10 col-content">
-                    <button type="button" class="btn btn-info collapsed" data-toggle="collapse" data-target="#instructions" aria-expanded="false">Show Instructions </button>
+                    <button type="button" class="btn btn-info collapsed" data-toggle="collapse" data-target="#instructions" aria-expanded="false">Mostrar instrucciones  </button>
                     <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> How to fill data to CSV file.
+                        <i class="fa fa-question-circle" aria-hidden="true"></i>  Rellenar datos en un archivo CSV.
                     </small>
                     <div id="instructions" class="collapse col-content" aria-expanded="false">
                         <img src="{{ asset('img/import_csv.png') }}" class="img-responsive">
