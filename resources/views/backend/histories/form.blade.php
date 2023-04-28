49222494303652a8d61568e537ccd11ba4c7b71d
Karla Azucena <karla29.vazquezl@gmail.com>
2023-04-28 09:21:39 -0600

    Actualizar

--- resources/views/backend/histories/form.blade.php
+++ resources/views/backend/histories/form.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Create and Update Histories Qr ' . Config::get('adminlte.title'))
 
 @section('content_header')
-    <h1>Histories Qr</h1>
+    <h1>Historial Qr</h1>
 @stop
 
 @section('content')
@@ -12,7 +12,7 @@
 
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">Add or Update</h3>
+            <h3 class="card-title">Agregar y actualizar</h3>
         </div>
 
         {{ Form::open(array('url' => route($data->form_action), 'method' => 'POST','autocomplete' => 'off', 'files' => true)) }}
@@ -22,12 +22,12 @@
 
             <div class="form-group row">
                 <div class="col-sm-2 col-form-label">
-                    <strong class="field-title">Name</strong>
+                    <strong class="field-title">Nombre</strong>
                 </div>
                 <div class="col-sm-10 col-content">
                     {{ Form::text('name', $data->name, array('class' => 'form-control', 'required')) }}
                     <small class="form-text text-muted">
-                        <i class="fa fa-question-circle" aria-hidden="true"></i> Name of empoyer or student.
+                        <i class="fa fa-question-circle" aria-hidden="true"></i> Su nombre.
                     </small>
                 </div>
             </div>
