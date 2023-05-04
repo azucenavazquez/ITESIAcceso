a6f980161cc010fea1b6763321d0363001e1719f
Karla Azucena <karla29.vazquezl@gmail.com>
2023-05-04 08:34:47 -0600

    Actualizar cambios

--- resources/views/backend/settings/form.blade.php
+++ resources/views/backend/settings/form.blade.php
@@ -27,7 +27,7 @@
                         </div>
                         <div class="col-sm-10 col-content">
                             {{ Form::text('start_time', $data->start_time, array('class' => 'form-control', 'required', 'id' => 'start_time')) }}
-                            <p class="form-text text-muted">Fill with the start time to enter the office</p>
+                            <p class="form-text text-muted">Rellene con la hora de inicio para entrar al plantel</p>
                         </div>
                     </div>
 
@@ -37,7 +37,7 @@
                         </div>
                         <div class="col-sm-10 col-content">
                             {{ Form::text('out_time', $data->out_time, array('class' => 'form-control', 'required', 'id' => 'out_time')) }}
-                            <p class="form-text text-muted">Fill with office hours</p>
+                            <p class="form-text text-muted">Llenarse con horas de oficina</p>
                         </div>
                     </div>
 
@@ -47,7 +47,7 @@
                         </div>
                         <div class="col-sm-10 col-content">
                             {{ Form::text('url', url('/'), array('class' => 'form-control', 'disabled', 'id' => 'url')) }}
-                            <p class="form-text text-muted">Your current URL. You cannot change this URL</p>
+                            <p class="form-text text-muted">Tu URL actual. No puede cambiar esta URL</p>
                         </div>
                     </div>
 
@@ -57,7 +57,7 @@
                         </div>
                         <div class="col-sm-10 col-content">
                             {{ Form::text('key_app', $data->key_app, array('class' => 'form-control', 'required', 'id' => 'key', 'readonly')) }}
-                            <p class="form-text text-muted">Application Key is used for communication with the Application. You can change the key by clicking on the button "Generate New Key" don't forget to save it</p>
+                            <p class="form-text text-muted">La clave de aplicación se utiliza para la comunicación con la aplicación. Puede cambiar la clave haciendo clic en el botón Generar nueva clave no olvide guardar</p>
                         </div>
                     </div>
 
@@ -67,7 +67,7 @@
                         </div>
                         <div class="col-sm-10 col-content">
                             {{ Form::select('timezone', $timezone, $data->timezone, array('id' => 'timezone', 'class' => 'form-control select2')) }}
-                            <p class="form-text text-muted">Fill in the Timezone you are</p>
+                            <p class="form-text text-muted">Rellena la zona horaria en la que te encuentras</p>
                         </div>
                     </div>
 
@@ -75,7 +75,7 @@
                 <div class="col-md-5">
                     <img class="img-responsive img-thumbnail" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl={{ $data->qr }}&choe=UTF-8" style="margin: 0 auto;display: block;">
                     <p class="text-center"><b>Código QR</b></p>
-                    <p class="text-center form-text text-muted">This QR code is used for the first time opening the App. <br>Scan this QR and this is done only once.</p>
+                    <p class="text-center form-text text-muted"> Este código QR se utiliza por primera vez abriendo la aplicación.<br>Escanee este QR y esto se hace solo una vez.</p>
                     <p class="text-center"><a href="https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl={{ $data->qr }}&choe=UTF-8" target="_blank"><button type="button" class="btn btn-success">Download</button></a></p>
                 </div>
             </div>
