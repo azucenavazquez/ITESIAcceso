cb875e3adeee1e57a9e35f5ac27147e46fb667eb
Karla Azucena <karla29.vazquezl@gmail.com>
2023-05-04 09:35:24 -0600

    Actualizar cambios

--- app/Http/Controllers/Backend/History/HistoryController.php
+++ app/Http/Controllers/Backend/History/HistoryController.php
@@ -138,7 +138,7 @@ class HistoryController extends Controller
         $data->form_action = $this->getRoute() . '.create';
         // Add page type here to indicate that the form.blade.php is in 'add' mode
         $data->page_type = 'add';
-        $data->button_text = 'Agregar';
+        $data->button_text = 'Import';
 
         return view('backend.histories.form', [
             'data' => $data,
